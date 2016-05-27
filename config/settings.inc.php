6eaf6c69e1e88ce269ac3a5f96e445c5a1cc15ba
ManuLEM <manuel.lemaire@gmail.com>
2016-05-27 15:51:14 +0200

    Adding settings files

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop_controle');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'ZSBKAmZo4yTiWoMQfgeJJiu7nd5qJ98Kmz4W6NzrrazAPjbcoC8c6ALa');
+define('_COOKIE_IV_', '5kOVuSRy');
+define('_PS_CREATION_DATE_', '2016-04-11');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.4');
+define('_RIJNDAEL_KEY_', 'G8yQ4vgK7zOZ8RaVkyKQdwQmxKXOb55I');
+define('_RIJNDAEL_IV_', 'pMnbrzvZCPRLytYvwPXeJg==');
